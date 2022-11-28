08ee618f4d5c5f26fd41f9ec82e811ff4011ac98
Gabriel Blake <61571088+gabrielblake28@users.noreply.github.com>
2022-11-27 22:44:10 -0500

    working on the script imports

--- scripts/api.js
+++ scripts/api.js
@@ -1,44 +1,40 @@
 async function fetchJSON(url) {
-  var image = {
-    // Default image values
-    img0: "./images/defaultCat0.jpg",
-    img1: "./images/defaultCat1.jpg",
-    img2: "./images/defaultCat2.jpg",
-    img3: "./images/defaultCat3.jpg",
-    img4: "./images/defaultCat4.jpg",
-  };
+  var image = {  // Default image values
+      "img0": './images/defaultCat0.jpg',
+      "img1": './images/defaultCat1.jpg',
+      "img2": './images/defaultCat2.jpg',
+      "img3": './images/defaultCat3.jpg',
+      "img4": './images/defaultCat4.jpg',
+  }
 
-  if (window.navigator.onLine == true) {
-    // If Online
-    let i = 0;
-    do {
-      res = await fetch(url); // Wait for api
-      imageData = await res.json(); // Wait for JSON
-      console.log(i + " https://cataas.com" + imageData.url); // Log loaded url
-      if (imageData.mimetype != "image/gif") {
-        // If image is not a gif
-        switch (
-          i // Set each image based on iteration
-        ) {
-          case 0:
-            image.img0 = "https://cataas.com" + imageData.url;
-            break;
-          case 1:
-            image.img1 = "https://cataas.com" + imageData.url;
-            break;
-          case 2:
-            image.img2 = "https://cataas.com" + imageData.url;
-            break;
-          case 3:
-            image.img3 = "https://cataas.com" + imageData.url;
-            break;
-          case 4:
-            image.img4 = "https://cataas.com" + imageData.url;
-            break;
-        }
-        i++; // Iterate
+  if (window.navigator.onLine == true) { // If Online
+      let i = 0;
+      do {
+          res = await fetch(url); // Wait for api
+          imageData = await res.json(); // Wait for JSON
+          console.log(i + ' https://cataas.com' + imageData.url); // Log loaded url
+          if (imageData.mimetype != 'image/gif') { // If image is not a gif
+              switch(i) { // Set each image based on iteration
+                  case 0:
+                      image.img0 = 'https://cataas.com' + imageData.url;
+                      break;
+                  case 1:
+                      image.img1 = 'https://cataas.com' + imageData.url;
+                      break;
+                  case 2:
+                      image.img2 = 'https://cataas.com' + imageData.url;
+                      break;
+                  case 3:
+                      image.img3 = 'https://cataas.com' + imageData.url;
+                      break;
+                  case 4:
+                      image.img4 = 'https://cataas.com' + imageData.url;
+                      break;
+              }
+              i++; // Iterate
+          }
       }
-    } while (i < 5 && window.navigator.onLine == true); // While there are less than 5 images and user remains online
+      while (i < 5 && window.navigator.onLine == true); // While there are less than 5 images and user remains online
   }
   // Return image object
   return image;
--- scripts/script.js
+++ scripts/script.js
@@ -2,57 +2,28 @@
 let isUnstable = false;
 const board = new GameCanvas();
 const player = new Player();
+player.anim = true;
 
 // Gameplay and Leaderboard forms
 let gameplay = document.getElementById("gameplay");
 let leaderboard = document.getElementById("leaderboard");
 // This array will be filled with server data on run
 let recordHolders = new Array(16).fill("---");
-let anim = true;
 
 // phrases to be said on matches
-let phrase = [
-  "Cat Related Phrase",
-  "Meow-velous Combo!",
-  "Purrrfect Match!",
-  "Endless Paws-ibilities!",
-  "It's Feeline Like a Good One!",
-  "Fun Fur Everyone!",
-  "No Dogs Allowed!",
-];
+let phrase = ["Cat Related Phrase", "Meow-velous Combo!", "Purrrfect Match!", "Endless Paws-ibilities!", "It's Feeline Like a Good One!", "Fun Fur Everyone!", "No Dogs Allowed!"];
 
 // EVENT LISTENERS
 
 // Listens for the redirect buttons to be pressed and reacts accordingly
-document
-  .getElementById("btn_redirect-leaderboard")
-  .addEventListener("click", () => {
-    gameplay.style.display = "none";
-    leaderboard.style.display = "block";
-    changePhrase();
-  });
-document
-  .getElementById("btn_redirect-gameplay")
-  .addEventListener("click", () => {
-    leaderboard.style.display = "none";
-    gameplay.style.display = "block";
-    changePhrase();
-  });
+document.getElementById("btn_redirect-leaderboard").addEventListener("click", () => { gameplay.style.display = "none"; leaderboard.style.display = "block"; changePhrase(); });
+document.getElementById("btn_redirect-gameplay").addEventListener("click", () => { leaderboard.style.display = "none"; gameplay.style.display = "block"; changePhrase(); });
 
 // Listens for start button press and reformats the page accordingly
-document.getElementById("btn_start").addEventListener("click", () => {
-  loadCanvas();
-  document.getElementById("btn_start").style.display = "none";
-  document.getElementById("btn_redirect-leaderboard").style.display = "none";
-  changePhrase();
-  document.getElementById("animation").style.display = "none";
-  document.getElementById("btn_restart").style.display = "block";
-});
+document.getElementById("btn_start").addEventListener("click", () => { loadCanvas(); document.getElementById("btn_start").style.display = "none"; document.getElementById("btn_redirect-leaderboard").style.display = "none"; changePhrase(); document.getElementById("animation").style.display = "none"; document.getElementById("btn_restart").style.display = "block"; document.getElementById("ui").style.color = "white"; });
 
 // Listens for reset button and refreshes the page from cache
-document.getElementById("btn_restart").addEventListener("click", () => {
-  window.location.reload(false);
-});
+document.getElementById("btn_restart").addEventListener("click", () => { window.location.reload(false); });
 
 // FUNCTIONS
 
@@ -61,23 +32,22 @@ function pageLoad() {
 }
 
 function changePhrase() {
-  document.getElementById("phrase").innerHTML =
-    phrase[Math.floor(Math.random() * phrase.length)];
+  document.getElementById("phrase").innerHTML = phrase[Math.floor(Math.random() * phrase.length)];
   if (isUnstable == true) {
     unstable();
   }
 }
 
+// Sets HTML phrases to indicate unstable build
 function unstable() {
-  document.getElementById("phrase").innerHTML = "UNSTABLE!";
+  document.getElementById("phrase").innerHTML = 'UNSTABLE!';
   document.getElementById("phrase").style.color = "red";
 }
 
 function toggleAnimations() {
-  anim = !anim;
-  if (anim == true) {
-    document.getElementById("phrase").style.animationIterationCount =
-      "infinite";
+  player.anim = !player.anim;
+  if (player.anim == true) {
+    document.getElementById("phrase").style.animationIterationCount = "infinite";
   } else {
     document.getElementById("phrase").style.animationIterationCount = "0";
   }
@@ -85,7 +55,7 @@ function toggleAnimations() {
 
 async function loadCanvas() {
   // Awaits JSON and images
-  var obj = await fetchJSON("https://cataas.com/cat?json=true&type=sq");
+  var obj = await fetchJSON('https://cataas.com/cat?json=true&type=sq');
 
   // Sets the value of each image in the object to the board array
   for (let n = 0; n < 5; n++) {
@@ -108,13 +78,16 @@ async function loadCanvas() {
     }
   }
 
+  // Resets the player's turn and score counters.
+  
   // !!! When adding writing to user's files, will include a portion for finding and setting the player's high score. !!!
   player.turnsLeft = 15;
+  player.score = 0;
 
   board.populateBoard();
 
-  // Will check for matches ten times per second.
-  window.setInterval(function () {
+  // Will check for matches four times per second.
+  window.setInterval(function() {
     board.checkForMatches();
     board.tileFall();
     board.repopulateBoard();
